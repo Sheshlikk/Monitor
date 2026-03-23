43e95861bfbcb3588f114f634a91c76f903b01a2
Sheshlikk-cm <adeptesao3@gmail.com>
2026-03-23 15:06:38 +0300

    Первый коммит

---
+++ ПР1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПР1
+{
+    internal class Program
+    {
+        public class Receipt
+        {
+           public int Number { get; set; }
+            public string Date { get; set; }
+            public float Summa { get; set; }
+
+            public Receipt(int number, string date, float summa) { 
+                Number = number;
+                Date = date;
+                Summa = summa;
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
